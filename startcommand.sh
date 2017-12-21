966d9b1659220ffacf9d641d9b96303e3fa18aca
leonidas <l.wallner@gmx.de>
2017-12-21 07:49:38 +0100

    xx

--- startcommand.sh
+++ startcommand.sh
@@ -1,2 +1,2 @@
-wget https://github.com/adwalo1/azure_scripts/raw/master/runme.sh ; chmod u+x runme.sh  ; ./runme.sh cpu_4core.txt
+/bin/bash -c "wget https://github.com/adwalo1/azure_scripts/raw/master/runme.sh ; chmod u+x runme.sh  ; ./runme.sh cpu_4core.txt"
 
